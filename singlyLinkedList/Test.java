3bb5cf21eb3005867005cc496dfea4b886957cf2
Leshan21 <leshanpasindu12@gmail.com>
2024-06-12 18:36:52 +0530

    addFirst

--- singlyLinkedList/Test.java
+++ singlyLinkedList/Test.java
@@ -7,3 +7,18 @@ class Node{
         this.next = null;
     }
 }
+
+class singlyLinkedList{
+    Node head;
+
+    void addFirst(int e){
+        Node temp = new Node(e);
+
+        if(head == null){
+            head = temp;
+        }else {
+            temp.next = head;
+            head = temp;
+        }
+    }
+}
